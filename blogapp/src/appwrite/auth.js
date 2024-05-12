f5fa8c46665a18052f32e3bb48af1829e07e7be5
Jenkibear <jenkinwork123@gmail.com>
2024-05-12 21:59:54 +1000

    auth service framework

---
+++ blogapp/src/appwrite/auth.js
@@ -0,0 +1,28 @@
+
+
+import conf from "../conf/conf.js";
+import {Client,Account} from "appwrite";
+
+export class AuthService{
+    client= new Client();
+    account;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId)
+        this.account=new Account(this.client)
+    }
+
+    async createAccount(){
+
+    }
+    async login(){
+        
+    }
+    async getCurrentUser(){
+        
+    }
+    async logout(){
+        
+    }
+
+}
