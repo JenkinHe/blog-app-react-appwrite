c9de37f9482b40cd4223acb2b28204abc4970e78
Jenkibear <jenkinwork123@gmail.com>
2024-05-12 22:30:46 +1000

    getpost method for databases

---
+++ blogapp/src/appwrite/config.js
@@ -0,0 +1,23 @@
+import { Client,Databases,Storage,Query } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service{
+    client =new Client()
+    databases;
+    bucket;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId)
+        this.databases=new Databases(this.client)
+        this.bucket=new Storage(this.client)
+    }
+
+    async getPost(slug){
+        try {
+           return await this.databases.getDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug)
+        } catch (error) {
+            console.log("Appwrite service ::getPost()::",error);
+            return false;
+        }
+    }
+}
