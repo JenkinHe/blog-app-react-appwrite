279aac6f679b8cb17aa2afd597d843d9246d62e3
Jenkibear <jenkinwork123@gmail.com>
2024-05-13 19:41:18 +1000

    routing for home and login with login requiring authentication

--- blogapp/src/main.jsx
+++ blogapp/src/main.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import {Provider }from 'react-redux'
+import store from './store/store.js'
+import Home from "./pages/Home.jsx"
+import Login from './pages/Login.jsx'
+import AuthLayout from "./components/AuthLayout.jsx"
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App />,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/login",
+        element:(
+          <AuthLayout authentication={false}>
+            <Login/>
+          </AuthLayout>
+        )
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <RouterProvider router={router} />
+    </Provider>
+    
   </React.StrictMode>,
 )
