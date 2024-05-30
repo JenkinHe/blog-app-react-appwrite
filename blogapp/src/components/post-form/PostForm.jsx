9a8602021b6f1fe9dad2c15d9d8860abd853c2fc
Jenkibear <jenkinwork123@gmail.com>
2024-05-30 17:43:24 +1000

    more input for the form

--- blogapp/src/components/post-form/PostForm.jsx
+++ blogapp/src/components/post-form/PostForm.jsx
@@ -63,6 +63,31 @@ export default function PostForm({post}){
                 />
 
             </div>
+            <div className="1/3 px-2">
+                <Input
+                label="Featured Image"
+                type="file"
+                className='mb-4'
+                accept="image/png, image/jpg, image/jpeg"
+                {...register("image",{required:!post})}
+                />
+                {post&&(
+                    <div className="w-full mb-4">
+                        <img src={appwriteService.getFilePreview(post.featuredImage)} alt={post.title} className="rounded-lg"/>
+                    </div>
+                )}
+                <Select
+                options ={["active","inactive"]}
+                label="Status"
+                className="mb-4"
+                {...register("status",{required:true})}
+                />
+                <Button
+                type="submit"
+                bgColor={post?"bg-green-500":undefined}
+                className="w-full"
+                >{post?"Update":"Submit"}</Button>
+            </div>
 
         </form>
     )
