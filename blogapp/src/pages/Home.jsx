5e52e50b714aa17ea8339caa52daa144d1830a07
Jenkibear <jenkinwork123@gmail.com>
2024-05-30 21:09:19 +1000

    all posts

--- blogapp/src/pages/Home.jsx
+++ blogapp/src/pages/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import appwriteService from '../appwrite/config'
+import { useState,useEffect } from 'react'
+import Container from '../components/container/Container'
+import PostCard from "../components/PostCard"
 
 export default function Home() {
   return (
