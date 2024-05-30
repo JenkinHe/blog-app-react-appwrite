cdbdb1bbca5109b3cd7c1cae133e2e48aca953dd
Jenkibear <jenkinwork123@gmail.com>
2024-05-30 21:04:32 +1000

    add post

--- blogapp/src/pages/AddPost.jsx
+++ blogapp/src/pages/AddPost.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Container from '../components/container/Container'
+import PostForm from '../components/post-form/PostForm'
 
 export default function AddPost() {
   return (
-    <div>AddPost</div>
+    <div className='py-6'>
+      <Container>
+        <PostForm/>
+      </Container>
+      
+    </div>
   )
 }
